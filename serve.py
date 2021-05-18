493aae40f8cb4ba9cd4682a94c477f464099afa3
Leonardo Uieda <leouieda@gmail.com>
2021-05-18 17:18:07 +0100

    Slides for the first part of the talk (#3)
    Includes a bit about the history, the new tools, and CSS tweaks

--- serve.py
+++ serve.py
@@ -7,5 +7,6 @@ from livereload import Server
 server = Server()
 server.watch("slide-contents.md")
 server.watch("index.html")
-server.watch("css/style.less")
+server.watch("css")
+server.watch("assets")
 server.serve(root=".", port="8008", host="localhost", open_url_delay=1)
